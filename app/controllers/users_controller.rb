77ee08987e0434bdc60ea701bddbdec4a97cac92
Faith <thao.faith2@gmail.com>
2021-09-18 13:53:59 -0600

    commit tree a70ff6e3c31e76b7b60b5d35a7c87192b38bfee0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
 
+  # def items_index
+  #   user = User.find(params[:user_id])
+  #   items = user.items
+  #   render json: items, include: :user
+  # end
+
+  # def item
+  #   item = Item.find(params[:id])
+  #   render json: item, include: :user
+  # end
+
+  # private
+
+  # def render_not_found_response
+  #   render json: { error: "Dog house not found" }, status: :not_found
+  # end
 end
